292154e92febe020be3431a7f64629ad4e65efff
yellowsunn <chohankook95@gmail.com>
2023-01-26 12:40:56 +0900

    불필요한 명령 제거

--- yellowsunn/k8s-1-26-1/master_node.sh
+++ yellowsunn/k8s-1-26-1/master_node.sh
@@ -50,17 +50,3 @@ kubectl completion bash >/etc/bash_completion.d/kubectl
 echo 'alias k=kubectl' >> ~/.bashrc
 echo "alias ka='kubectl apply -f'" >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
-
-# git clone k8s-code
-git clone https://github.com/sysnet4admin/_Lecture_k8s_learning.kit.git
-mv /home/vagrant/_Lecture_k8s_learning.kit $HOME
-find $HOME/_Lecture_k8s_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
-
-# make rerepo-k8s-learning.kit and put permission
-cat <<EOF > /usr/local/bin/rerepo-k8s-learning.kit
-#!/usr/bin/env bash
-rm -rf $HOME/_Lecture_k8s_learning.kit 
-git clone https://github.com/sysnet4admin/_Lecture_k8s_learning.kit.git $HOME/_Lecture_k8s_learning.kit
-find $HOME/_Lecture_k8s_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
-EOF
-chmod 700 /usr/local/bin/rerepo-k8s-learning.kit
